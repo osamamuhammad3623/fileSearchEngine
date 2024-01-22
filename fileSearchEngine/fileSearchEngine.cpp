e76992f08e249277bae1f8ab350b1684002c30ec
osamaMuhammad <osamamohammed3623@gmail.com>
2024-01-22 19:25:45 +0200

    implementation of search function

--- fileSearchEngine/fileSearchEngine.cpp
+++ fileSearchEngine/fileSearchEngine.cpp
@@ -57,7 +57,73 @@ vector<pair<string,string>> FileSearchEngine::getResult(void){
 }
 
 void FileSearchEngine::search(void){
+    if(status == Status_OK){
 
+        vector<fs::directory_entry> content;
+        if(filters.recursive == 1){
+            for(auto entry: fs::recursive_directory_iterator(dirPath)){
+                content.push_back(entry);
+            }
+        }else{
+            for(auto entry: fs::directory_iterator(dirPath)){
+                content.push_back(entry);
+            }
+        }
+
+        for(auto entry: content){
+
+            if((fs::is_directory(entry) == 1) && (filters.directoryName == 1)){
+
+                if(findKeyword(entry.path().filename().u8string())){
+                    result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                }
+
+            }else if (fs::is_regular_file(entry) == 1){
+
+                if(filters.fileName == 1){
+                    if(findKeyword(entry.path().filename().u8string())){
+                        result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                    }
+                }
+
+                if(filters.infile == 1){
+                    string data = readFile(entry.path().u8string());
+                    if(findKeyword(data)){
+                        result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                    }
+                }
+
+                if((filters.headerfile == 1)  && (entry.path().extension() == ".h")){
+                    if(findKeyword(entry.path().filename().u8string())){
+                        result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                    }
+                }
+
+                if((filters.cPlusPlus == 1)   && ((entry.path().extension() == ".cpp") || (entry.path().extension() == ".c"))){
+                    if(findKeyword(entry.path().filename().u8string())){
+                        result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                    }
+
+                }
+
+                if((filters.python == 1) && (entry.path().extension() == ".py")){
+                    if(findKeyword(entry.path().filename().u8string())){
+                        result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                    }
+
+                }
+
+                if((filters.txt == 1) && (entry.path().extension() == ".txt")){
+                    if(findKeyword(entry.path().filename().u8string())){
+                        result.push_back(make_pair(entry.path().filename().u8string(), entry.path().u8string()));
+                    }
+                }
+
+            }
+        }
+
+        status = Status_Search_Completed;
+    }
 }
 
 EntryType FileSearchEngine::getEntryType(string path){
